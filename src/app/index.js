9efa279e75e75d68197d8f2b12db22538d5d91d5
Maxxen <max@gabrielsson.com>
2018-09-09 22:11:38 +0200

    Added initial react hello world component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,16 @@
 console.log("Hello world");
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+class Layout extends React.Component {
+    render() {
+        return (
+            <h1>Hello World</h1>
+        );
+    }
+}
+
+const app = document.getElementById("app");
+
+ReactDOM.render(<Layout/>, app);
